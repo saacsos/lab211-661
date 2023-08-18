52710b2ced5b9c881717cfb862abdd8c104c52b9
Saac Sornchai <sornchai.la@ku.th>
2023-08-18 11:00:38 +0700

    use services FXRouter

--- src/main/java/ku/cs/lab/HelloApplication.java
+++ src/main/java/ku/cs/lab/HelloApplication.java
@@ -4,17 +4,20 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import ku.cs.services.FXRouter;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-    @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        FXRouter.bind(this, stage, "Hello World", 1024, 768);
+        configRoute();
+        FXRouter.goTo("hello");
+    }
+
+    private void configRoute() {
+        String viewPath = "ku/cs/views/";
+        FXRouter.when("hello", viewPath + "hello-view.fxml");
     }
 
     public static void main(String[] args) {
